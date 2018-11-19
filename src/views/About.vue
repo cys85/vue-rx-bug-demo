df76acb1e8a7b62e9b0eb3e20c24de856e05b87d
cys85 <kuerbb@aliyun.com>
2018-11-19 10:09:35 +0800

    first commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>{{timestamp}}</h1>
   </div>
 </template>
+
+<script lang="ts">
+import { Component, Vue, Mixins } from 'vue-property-decorator';
+import myMixin from './myMixin';
+import myMixin2 from './myMixin2';
+import { of } from 'rxjs';
+import {tap} from 'rxjs/operators';
+
+@Component({
+  subscriptions() {
+
+    // tslint:disable-next-line:no-console
+    console.log('subscriptions');
+    return {
+      timestamp: of(+new Date()).pipe(tap(console.log)),
+    };
+  },
+})
+export default class About extends Mixins(myMixin, myMixin2) {}
+</script>
